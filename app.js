9fd3c4ef157e1ec5a700860797c8df0ad6cfa66e
Vitor Jesus <vialves29@gmail.com>
2024-08-26 00:46:47 -0300

    novo

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Oi, sua aplicacao esta ok')
+})
+
+app.get('/carros', (req, res)=>{
+    res.status(200).send('Oi, sua aplicacao esta ok na rota CARROS')
+})
+
+
+app.get('/outra', (req, res)=>{
+    res.status(200).send('Oi, sua aplicacao esta ok na rota OUTRA')
+})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
+})
